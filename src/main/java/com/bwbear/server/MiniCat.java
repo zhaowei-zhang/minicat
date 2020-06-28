72bf5680ef98062eb07cd55912c23a48d46f02fb
zhaowei.zhang <zhaowei.zhang01@HAND-CHINA.COM>
2020-06-28 09:35:46 +0800

    1.3 多线程实现

--- src/main/java/com/bwbear/server/MiniCat.java
+++ src/main/java/com/bwbear/server/MiniCat.java
@@ -102,24 +102,29 @@ public class MiniCat {
 //            response.outputHtml(request.getUrl());
 //            socket.close();
 //        }
-
+        //3.0
+//        while(true) {
+//            Socket socket = serverSocket.accept();
+//            InputStream inputStream = socket.getInputStream();
+//
+//            // 封装Request对象和Response对象
+//            Request request = new Request(inputStream);
+//            Response response = new Response(socket.getOutputStream());
+//
+//            // 静态资源处理
+//            if(servletMap.get(request.getUrl()) == null) {
+//                response.outputHtml(request.getUrl());
+//            }else{
+//                // 动态资源servlet请求
+//                HttpServlet httpServlet = servletMap.get(request.getUrl());
+//                httpServlet.service(request,response);
+//            }
+//            socket.close();
+//        }
         while(true) {
             Socket socket = serverSocket.accept();
-            InputStream inputStream = socket.getInputStream();
-
-            // 封装Request对象和Response对象
-            Request request = new Request(inputStream);
-            Response response = new Response(socket.getOutputStream());
-
-            // 静态资源处理
-            if(servletMap.get(request.getUrl()) == null) {
-                response.outputHtml(request.getUrl());
-            }else{
-                // 动态资源servlet请求
-                HttpServlet httpServlet = servletMap.get(request.getUrl());
-                httpServlet.service(request,response);
-            }
-            socket.close();
+            RequestProcessor requestProcessor = new RequestProcessor(socket,servletMap);
+            requestProcessor.start();
         }
 
     }
